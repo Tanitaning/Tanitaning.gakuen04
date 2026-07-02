9e3619f75e267da3f63b037829559c31aafea3e4
Tanitaning <misaki_taniguchi@mail.toyota.co.jp>
2026-07-03 08:50:23 +0900

    Add files via upload

--- 5なぜ/search.js
+++ 5なぜ/search.js
@@ -19,39 +19,47 @@ function searchToBrowser(){
     const joukyou = data.joukyou
     const hinmei = data.hinmei
     const cause = data.sinnin
+    const place = data.occurrencePlace
 
 
     // 表示したい要素を追加したい
 
-    const div4 = document.createElement("div4")
-    div4.id = "days";
-    div4.textContent = Date;
-    const divd = document.querySelector("#day");
-    divd.appendChild(div4);
-
-    const div5 = document.createElement("div5")
-    div5.id = "hin";
-    div5.textContent = hinmei;
-    const divd = document.querySelector("#name");
-    divd.appendChild(div5);
-
+    
     const div1 = document.createElement("div1")
     div1.id = "test";
     div1.textContent = joukyou;
     const divaaaaa = document.querySelector("#jk");
     divaaaaa.appendChild(div1);
-
+    
     const div2 = document.createElement("div2")
     div2.id = "tests";
     div2.textContent = naiyou;
     const divbbbbb = document.querySelector("#ny");
     divbbbbb.appendChild(div2);
-
+    
     const div3 = document.createElement("div3")
     div3.id = "testss";
     div3.textContent = cause;
     const divccccc = document.querySelector("#si");
     divccccc.appendChild(div3);
+    
+    const div4 = document.createElement("div4")
+    div4.id = "days";
+    div4.textContent = Date;
+    const divd = document.querySelector("#day");
+    divd.appendChild(div4);
+    
+    const div5 = document.createElement("div5")
+    div5.id = "hin";
+    div5.textContent = hinmei;
+    const dive = document.querySelector("#name");
+    dive.appendChild(div5);
+
+    const div6 = document.createElement("div6")
+    div6.id = "basyoo";
+    div6.textContent = place;
+    const divf = document.querySelector("#basyo");
+    divf.appendChild(div6);
 
     //上記divに対して、17行目の値を代入したい 
 }
